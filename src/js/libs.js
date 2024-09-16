1cfc0ea45654998d67ba54f9acf3d807595c1dab
chocopie777 <egoran99@gmail.com>
2024-09-16 15:14:37 +0500

    reviews-slider

--- src/js/libs.js
+++ src/js/libs.js
@@ -1,1 +1,10 @@
-// js из node_modules
+// js из node_moduless
+import $ from "jquery";
+import 'slick-carousel/slick/slick.js';
+
+$('.review-slider').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,40 @@
 import './libs.js';
+
+//получаем все кнопки
+const tabsBtn = document.querySelectorAll('.tabs__nav-btn');
+//получаем все элеменетам
+const tabsItems = document.querySelectorAll('.tabs__item');
+
+//проходимся по всем кнопкам
+tabsBtn.forEach(onTabClick);
+
+//функция обработчик клика по кнопке
+function onTabClick(item) {
+    //вешаем обработчик события на кнопку
+    item.addEventListener('click', function () {
+        //ссылка на текущую кнопку
+        let currentBtn = item;
+        //получаем содержимое атрибута текущей кнопки
+        let tabId = currentBtn.getAttribute('data-tab');
+        //получаем ссылку на элемент относящейся к текущей кнопке
+        let currentTab = document.querySelector(tabId);
+
+        //проверка на то чтобы не добавлять классы если кнопка уже была активна ранее
+        if (!currentBtn.classList.contains('active')) {
+            //проходимся по всем кнопкам и удаляем им класс active
+            tabsBtn.forEach(function (item) {
+                item.classList.remove('active');
+            });
+
+            //проходимся по всем элементам и удаляем им класс active
+            tabsItems.forEach(function(item) {
+                item.classList.remove('active');
+            })
+
+            //добавляем класс active текущей кнопке
+            currentBtn.classList.add('active');
+            //добавляем класс active текущему элементу
+            currentTab.classList.add('active');
+        }
+    });
+}
